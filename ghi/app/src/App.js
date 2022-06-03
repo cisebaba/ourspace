97ad7ba55e3ae6bb9371e994be7fdc5e6b28e0ca
Amanda <apkiehm@gmail.com>
2022-06-02 19:01:21 -0600

    Commented out React app file

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import ListJobs from './JobsPage';
+// import ListJobs from './JobsPage';
+
+// Still working on JobsPage - not functioning yet
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<ListJobs />} />
+          {/* <Route path="/" element={<ListJobs />} /> */}
         </Routes>
       </BrowserRouter>
     </>
