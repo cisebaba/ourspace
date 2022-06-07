862d26878ad2aaab47a04427061f3fc34a8dc4d9
Amanda <apkiehm@gmail.com>
2022-06-06 20:33:21 -0600

    Progress on job list

--- ghi/app/src/Jobs/JobsList.js
+++ ghi/app/src/Jobs/JobsList.js
@@ -1,5 +1,35 @@
 import React, { useState, useEffect } from "react";
 
+// function JobColumn(props) {
+//   return (
+//     <div className="col">
+//       {props.list.map(data => {
+//         const conference = data.conference;
+//         return (
+//           <div key={conference.href} className="card mb-3 shadow">
+//             <img src={conference.location.picture_url} className="card-img-top" />
+//             <div className="card-body">
+//               <h5 className="card-title">{conference.name}</h5>
+//               <h6 className="card-subtitle mb-2 text-muted">
+//                 {conference.location.name}
+//               </h6>
+//               <p className="card-text">
+//                 {conference.description}
+//               </p>
+//             </div>
+//             <div className="card-footer">
+//               {new Date(conference.starts).toLocaleDateString()}
+//               -
+//               {new Date(conference.ends).toLocaleDateString()}
+//             </div>
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// }
+
+
 function JobsList() {
   const [jobs, setJobs] = useState([]) ;
 
