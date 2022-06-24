de80a419c468390db028dd0197dfbbad67ff0d8d
Starr Solis <starr.solis7@gmail.com>
2022-06-24 17:05:26 -0500

    flake8

--- events/api/test.py
+++ events/api/test.py
@@ -5,7 +5,6 @@ from events_app.models import Event, Location, State
 from django.urls import reverse
 
 
-
 class BasicTest(TestCase):
 
     def test_list(self):
@@ -13,20 +12,21 @@ class BasicTest(TestCase):
         response = client.get(reverse("api_list_events"))
         self.assertEquals(response.status_code, 200)
 
-
     def test_fields(self):
         event = Event()
         event.name = "Event"
-        event.starts= datetime(2000,1,1)
-        event.ends= datetime(2000,2,1)
+        event.starts = datetime(2000, 1, 1)
+        event.ends = datetime(2000, 2, 1)
         event.description = "Description"
 
-        state = State.objects.create(id = 1, name = "Kansas", abbreviation = "KS")
-        location = Location.objects.create(name ="A Place", city ="Austin", state = state)
+        state = State.objects.create(id=1, name="Kansas", abbreviation="KS")
+        location = Location.objects.create(
+            name="A Place",
+            city="Austin",
+            state=state
+        )
         event.location = location
         event.save()
-      
+
         record = Event.objects.get(pk=1)
         self.assertEqual(record, event)
-
-    
